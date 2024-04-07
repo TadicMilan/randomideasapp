0ce3e6bd10c8e3feaa1cfe3e3b3764ecfb0241ba
iSeeCookie <milan-tad@hotmail.com>
2024-04-07 17:21:01 +0200

    Security update, prevent XSS, HPP

--- server.js
+++ server.js
@@ -4,10 +4,35 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
 connectDB();
 
 const app = express();
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security header
+app.use(helmet());
+
+//Prevent Cross Site Scripting attacks
+app.use(xss());
+
+//Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100
+});
+
+app.use(limiter);
+
+//Prevent http param pollution
+app.use(hpp());
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,7 +45,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors({
     origin: ['http://localhost:5000', 'http://localhost:3000'],
     credentials: true
-}))
+}));
 
 app.get('/', (request, response) => {
     response.json({ message: 'Welcome to the RandomIdeasAPI'});
